8fddc6915cfdde64efcec501fcca8424c375e346
Taco <79318163+tacotopasu@users.noreply.github.com>
2022-07-24 16:22:59 +0100

    New automatic setup (WIP)
    -Improved Sleep Mode
    -Detailed Error Logger added
    -Fixed small typo in settings.ini
    -Removed unused lines and optimized functions.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Discord-RichPresence
- A Rich Presence Application that uses the pypresence library
+# Taco's RPC
+
+🚀A Discord Custom Rich Presence Application
+
+[![pypresence](https://img.shields.io/badge/using-pypresence-00bb88.svg?style=for-the-badge&logo=python&logoWidth=20)](https://github.com/qwertyquerty/pypresence)
+
+✅ Download the Latest Release here: <a href="https://github.com/tacotopasu/latestreleaselinkhere"  target="_BLANK">Latest Release</a>
+
+  <p align="center">
+   Made with ❤️ by <a href="https://twitter.com/tacotopasu"><img alt="Twitter Follow" src="https://img.shields.io/twitter/follow/tacotopasu?style=social"> </a>
+  </p>
+
+
+## Features
+
+🔥 Made in Python, using the PyPresence Library.
+
+📖 It's Open Source!
+
+⛓️ Easy to Share your RPC with others!
+
+🔌 Requires Internet Access.
+
+🖥️ Clickable GUI!
+
+📠 Lots of customization avaliable!
+
+
+## 🤝 Contributing
+
+All contributions, issues and feature requests are welcome! 😍
+
+## Show your support
+
+Give a ⭐️ if this project helped you! 🥰
+
+If you like this app , Star it on Github, Follow me on Twitter!
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
+from tracemalloc import stop
 from pypresence import Presence as DiscordRichPresence
 from configparser import ConfigParser
-import time, os
+import time, os, psutil, requests, threading, multiprocessing
 
 keepGoing = True
 Connected = False
-
-# Load config
-config = ConfigParser()
-config.read("settings.ini")
-
-#Get all the needed vars
-richpresence = config["TEXT"]["rpc"]
-rpc = DiscordRichPresence(richpresence, pipe = 0)
+os.system("")
 
 def Setup():
     # Load config
@@ -29,51 +23,175 @@ def Setup():
     sleeping = config["SLEEPING"]
     timee = time.time()
     
+def PPrint(status, text): # Easier method for printing out better looking warnings and errors
+    if status == 'warn':
+        status = '\033[33m [Warn]'
+    elif status == 'error':
+        status = '\033[31m[Error]'
+    elif status == 'fatal':
+        status = '\033[31m[Fatal]'
+    else:
+        status = '\033[35m [Info]'
+    
+    print("\033[0m\033[1m" + status + f"\033[0m \033[1m{text}")
+
+
+config = ConfigParser()
+config.read("settings.ini")
+# Try to read settings.ini, if file isn't there, create template file
+myPath = os.path.realpath(__file__)
+completeName = os.path.join(myPath[:-8], 'settings.ini') 
+os.system(f'cd {myPath[:-8]}')
+if not os.path.exists(completeName):
+    PPrint('fatal', "Couldn't find 'settings.ini'.")
+    PPrint('info', 'Creating file...')
+    response = requests.get('https://raw.githubusercontent.com/tacotopasu/Discord-RichPresence/main/settings.ini')
+    open("settings.ini", "wb").write(response.content)
+    PPrint('info', 'File successfully created.')
+    PPrint('info', 'Running setup...')
+    myPath = os.path.realpath(__file__)
+    completeName = os.path.join(myPath[:-8], 'setup.py') 
+    os.system(f'cd {myPath[:-8]}')
+    if not os.path.exists(completeName):
+        response = requests.get('https://raw.githubusercontent.com/tacotopasu/Discord-RichPresence/main/setup.py')
+        open(completeName, "wb").write(response.content)
+    os.system(completeName)
+    settingsInstalled = True
+
+
+myPath = os.path.realpath(__file__)
+completeName = os.path.join(myPath[:-8], 'setup.py') 
+if not os.path.exists(completeName):
+    os.system(f'cd {myPath[:-8]}')
+    response = requests.get('https://raw.githubusercontent.com/tacotopasu/Discord-RichPresence/main/setup.py')
+    open(completeName, "wb").write(response.content)
+    os.system(completeName)
+
+
+try:
+    richpresence = config["TEXT"]["rpc"]
+except:
+    PPrint('fatal', "Couldn't get 'Discord Application ID' from 'settings.ini'.")
+    PPrint('info', "Get your Discord Application ID at 'https://discord.com/developers/applications'.")
+    input('\nPress Enter to close.')
+    exit()
+# Try to use RPC's Client ID
+try:
+    PPrint('info', 'Initializing RPC...')
+    rpc = DiscordRichPresence(richpresence, pipe = 0)
+except:
+    PPrint('fatal', "Couldn't connect using the Discord Application ID that was set in 'settings.ini'.")
+    PPrint('info', "This can be caused by a lack of connection to the internet or an invalid Discord Application ID.")
+    PPrint('info', "Check your Internet connection and if the error persists run 'setup.py'.")
+    PPrint('info', "If you haven't yet, get your Discord Application ID at 'https://discord.com/developers/applications'.")
+    input('\nPress Enter to close.')
+    exit()
+
+
+
+def Logo():
+    print("\u001b[35m _____               _        __    ___  ___ \n/__   \__ _  ___ ___( )__    /__\  / _ \/ __\ \n  / /\/ _` |/ __/ _ \/ __|  / \// / /_)/ /  \n / / | (_| | (_| (_) \__ \ / _  \/ ___/ /___ \n \/   \__,_|\___\___/|___/ \/ \_/\/   \____/\033[0m")
+
+
+def Sleep():
+    os.system('cls')
+    Logo()
+    rpc.update(details= sleeping["details"], state= sleeping["state"], large_image= sleeping["large"], large_text= sleeping["largeText"])
+    print('\n')
+    PPrint('', 'Currently sleeping...')
+    PPrint('', 'Press Enter to wake up.')
+    input()
+    os.system('cls')
+    ConnectRPC(secondButton, "Just woke up...", Connected)
+
+stopStats = 0
+def Stats(): # Loops forever, I'll finish later...
+    killMe = stopStats
+    while killMe == 0:
+        cpu_per = round(psutil.cpu_percent(),1) # Get CPU Usage
+        # mem = psutil.virtual_memory() # Get Ram Usage
+        mem_per = round(psutil.virtual_memory().percent,1) # Get Ram Usage in %
+        rpc.update(details= "Taco's PC Status", state="CPU "+str(cpu_per)+"%"+" / RAM "+str(mem_per)+"%")
+        killMe = stopStats
+        time.sleep(5)
+        
 
 # Connect RPC Function 
 def ConnectRPC(buttonState, game, con):
-
+    modes = ['ramen', 'sleep', 'stats']
     Setup()
-    if(not con):
-        rpc.connect()
-    if(game == ""):
+    if not con:
+        try:
+            rpc.connect()
+            PPrint('info', 'RPC Connected.')
+            time.sleep(1)
+        except:
+            PPrint('fatal', "Couldn't connect RPC. Check if the Application ID is correct (Located in settings.ini).")
+            PPrint('', 'Press Enter to close.')
+            input()
+            exit()
+    if game == "": # Default State ('game' var is empty)
         statee = text["state"]
+        if buttonState == "False":            
+            rpc.update(details= text["details"], state= statee, large_image= image["largeImage"],
+            large_text=text["imageText"], start= timee, small_image = image["smallImage"], small_text = text["smallText"],
+            buttons=[{"label": button1["label"], "url": button1["url"]}])
+        elif buttonState == "True":
+            rpc.update(details= text["details"], state= statee, large_image= image["largeImage"],
+            large_text=text["imageText"], start= timee, small_image = image["smallImage"], small_text = text["smallText"],
+            buttons=[{"label": button1["label"], "url": button1["url"]},
+                     {"label": button2["label"], "url": button2["url"]}])
     else:
-        statee = game
+        if game.lower() not in modes:
+            statee = game
+            if buttonState == "False":            
+                rpc.update(details= text["details"], state= statee, large_image= image["largeImage"],
+                large_text=text["imageText"], start= timee, small_image = image["smallImage"], small_text = text["smallText"],
+                buttons=[{"label": button1["label"], "url": button1["url"]}])
+            elif buttonState == "True":
+                rpc.update(details= text["details"], state= statee, large_image= image["largeImage"],
+                large_text=text["imageText"], start= timee, small_image = image["smallImage"], small_text = text["smallText"],
+                buttons=[{"label": button1["label"], "url": button1["url"]},
+                         {"label": button2["label"], "url": button2["url"]}])
+            
 
-    if(buttonState == "False"):            
-        rpc.update(details= text["details"], state= statee, large_image= image["largeImage"],
-        large_text=text["imageText"], start= timee, small_image = image["smallImage"], small_text = text["smallText"],
-        buttons=[{"label": button1["label"], "url": button1["url"]}])
-    elif(buttonState == "True"):
-        rpc.update(details= text["details"], state= statee, large_image= image["largeImage"],
-        large_text=text["imageText"], start= timee, small_image = image["smallImage"], small_text = text["smallText"],
-        buttons=[{"label": button1["label"], "url": button1["url"]},
-                 {"label": button2["label"], "url": button2["url"]}])
+    # Ramen Cafe
+    if game.lower() == "ramen":          
+        rpc.update(details= "Ramen Cafe", state= "Discord Server", large_image= "ramen",
+            large_text= "Ramen Cafe - Discord Server",
+            buttons=[{"label": "Join now!", "url": "https://discord.st/ramen"}])
+
+    # Sleeping
+    if game.lower() == "sleep":           
+        Sleep()
+
+    
+    # PC Status
+    #if game.lower() == "stats":
+    #    warning = False
+    #    if(not warning):
+    #        PPrint('info', "This is a new feature, and since bugs are expected feel free to report any errors to 'https://github.com/tacotopasu/Discord-RichPresence/issues'.")
+    #        warning = True
+    #    Stats()
+    #    # Bad method but works fine for now
+    #    PPrint('', 'Press Enter to stop stats.')
+    #    input()
+    #    global stopStats
+    #    stopStats = 1
+    #    ConnectRPC(secondButton, "", Connected)
+
+
 
 Setup()
 ConnectRPC(secondButton, "", Connected)
 Connected = True
 
 
-def Logo():
-    print("\033[35m _____               _        __    ___  ___ \n/__   \__ _  ___ ___( )__    /__\  / _ \/ __\ \n  / /\/ _` |/ __/ _ \/ __|  / \// / /_)/ /  \n / / | (_| | (_| (_) \__ \ / _  \/ ___/ /___ \n \/   \__,_|\___\___/|___/ \/ \_/\/   \____/\033[0m")
-
-while(keepGoing):
+while keepGoing:
     os.system('cls')
     Logo()
-    choice = input("\n\n\033[1mWhat game are you playing right now? (Press Enter to close.)\n> ")
-    if(choice == ""):
+    choice = input("\n\nWhat game are you playing right now? (Press Enter to close.)\n> ")
+    if choice == "":
         keepGoing = False
-   
-# Example usage:
-    elif(choice.lower() == "ramen"):          
-        rpc.update(details= "Ramen Cafe", state= "Discord Server", large_image= "ramen",
-            large_text= "Ramen Cafe - Discord Server",
-            buttons=[{"label": "Join now!", "url": "https://discord.st/ramen"}])
-    
-    elif(choice.lower() == "sleeping" or choice.lower() == "sleep"):           
-        rpc.update(details= sleeping["details"], state= sleeping["state"], large_image= sleeping["large"], large_text= sleeping["largeText"])
-    
     else:
         ConnectRPC(secondButton, choice, Connected)
